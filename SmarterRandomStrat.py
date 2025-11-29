1b54e97e7516ec29dc04fc290cd33c50735a5e3c
Sophia Wang <sophia.xy.wang@gmail.com>
2025-11-28 21:43:37 -0800

    Revert "Fix move selection so that ants chose random moves that are valid"
    This reverts commit 194b5e31a75f4fe5f1d3e61eec877516a4c13581.

--- SmarterRandomStrat.py
+++ SmarterRandomStrat.py
@@ -11,15 +11,6 @@ class SmarterRandomStrat(AntStrategy):
         self.inbox = []
         self.outbox = []
         self.direction = "WEST"
-        self.cardinals = { "NORTH": (1, 0),
-                "SOUTH": (1, 2),
-                "EAST": (2, 1),
-                "WEST": (0, 1),
-                "NORTHEAST": (2, 0),
-                "SOUTHEAST": (2, 2),
-                "SOUTHWEST": (0, 2),
-                "NORTHWEST": (0, 0),
-                "HERE": (1,1) }
 
     def receive_info(self, messages):
         '''Receive messages sent by teammates in the last round.'''
@@ -31,20 +22,7 @@ class SmarterRandomStrat(AntStrategy):
         self.outbox = []
         return to_return
 
-    def valid_move(self, vision, moves):
-        '''Check if a move is valid from the moves available.'''
-        while (len(moves) > 0):
-            move = random.choice(moves)
-            coords = self.cardinals[move]
-            if ("#" not in vision[coords[0]][coords[1]]):
-                return move
-            else:
-                moves.remove(move)
-
-        return 'PASS'
-
     def one_step(self, x, y, vision, food):
-        
         '''Calculate and return a randomly chosen, but valid, next move.'''
         cardinals = { "NORTH": (1, 0),
                 "SOUTH": (1, 2),
@@ -69,10 +47,13 @@ class SmarterRandomStrat(AntStrategy):
         # Take ant towards hill if it's carrying food
         if food:
             if self.anthill == "@":
-                return self.valid_move(vision, ["NORTH", "NORTHEAST", "EAST", "WEST", "NORTHWEST"])
+                return random.choice(["NORTH", "NORTHEAST", "EAST", "WEST", "NORTHWEST"])
             else:
-                return self.valid_move(vision, ["SOUTH", "SOUTHEAST", "EAST", "WEST", "SOUTHWEST"])
+                return random.choice(["SOUTH", "SOUTHEAST", "EAST", "WEST", "SOUTHWEST"])
 
         # Otherwise move randomly
         while True:
-            return self.valid_move(vision, ["NORTH", "NORTHEAST", "EAST", "SOUTHEAST", "SOUTH", "SOUTHWEST", "WEST", "NORTHWEST"])
+            move = random.choice(["NORTH", "NORTHEAST", "EAST", "SOUTHEAST", "SOUTH", "SOUTHWEST", "WEST", "NORTHWEST"])
+            coords = cardinals[move]
+            if "#" not in vision[coords[0]][coords[1]]:
+                return move
